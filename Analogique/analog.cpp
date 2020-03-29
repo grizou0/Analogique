6c5257746217523f8007034c4a6d12f6f9a9809e
Jean-Pierre Olemans <43502627+grizou0@users.noreply.github.com>
2020-03-29 15:34:20 +0200

    Add files via upload

--- Analogique/analog.cpp
+++ Analogique/analog.cpp
@@ -1,21 +1,23 @@
 // Source lecture analogique
 // IC: MCP3008
 // ------------
-// 16 VDD 3.3V  15 VRef:3.3V
-// 14 AGND: GND 13 CLK
-// 12 DOUT      11 DIN 
-// 10 CS         9 DGND GND
+// Chan0  |1    16|VDD 3.3V
+// Chan1  |2    15|Vref 3.3V
+// Chan2  |3    14|AGND 0V
+// Chan3  |4    13|CLK
+// Chan4  |5    12|DOUT (MOSI Raspberry Pi)
+// Chan5  |6    11|DIN  (MISO Raspberry Pi)
+// Chan6  |7    10|CS
+// Chan7  |8     9|GND
+
 #include <wiringPi.h>
 #include <stdio.h>
 #include <stdlib.h>
-#define MOSI  12  //Physical 19 read
-#define MISO  13  //         21 Send
-#define Clk   28  //  22       23
-#define CE    29 //          40
+#define MOSI  12  // Physical 19 read
+#define MISO  13  //          21 Send 
+#define Clk   28  //          38
+#define CE    29 //           40
 
-//#define LED1   29    // pin 40
-//#define LED2   28    // pin 38
-//#define LED3   27    // pin 36
 void pulse()
 {
 digitalWrite(Clk,0);
@@ -31,14 +33,13 @@ int data,value;
 
 	// Calling wiringPi setup first.
 	wiringPiSetup();
-	pinMode(MOSI, INPUT); 
-	pinMode(MISO, OUTPUT);
-	pinMode(Clk, OUTPUT);
+	pinMode(MOSI, INPUT); // Input Raspberry To DOUT
+	pinMode(MISO, OUTPUT);// Output Raspberry To DIN
+	pinMode(Clk, OUTPUT); 
 	pinMode(CE , OUTPUT);
 	delay(200);
-
 // data Start + Single + D2 + D1 + D0	  
-//     1         1        0    0    0	  
+//     1         1        0    0    0	  (chan 0)
 	digitalWrite(MISO,0);
 	digitalWrite(Clk,0);
 	digitalWrite(CE,1);
@@ -49,24 +50,23 @@ int data,value;
 	      pulse();
 	      digitalWrite(MISO,1); //Start
               pulse();
-              digitalWrite(MISO,1);
+              digitalWrite(MISO,1); //SGN/DIFF
               pulse();
-              digitalWrite(MISO,0);
+              digitalWrite(MISO,0); // D2
               pulse();
-              digitalWrite(MISO,0);
+              digitalWrite(MISO,0); // D1
               pulse();
-	      digitalWrite(MISO,0);
+	      digitalWrite(MISO,0); // D0
 	      pulse();
 	   while(digitalRead(MOSI)) pulse(); //Attend le start bit  
            data=0;
 	   value=0;
 	   for(int i=0;i<21;i++) // lecture 1 bit vide, 10 data, 1 bit null
-	     { // 00 0000000010 100000000  Data:1280   Value:2
-	       // 00 1100110111 110110011  Data:421811   Value:823
+	     { // 00 1100110111 110110011  Data:421811   Value:823
 	       //    9876543210 123456789  21 bits de tramsission 
 	     pulse();
 	     data <<=1;
-	     if(i<12) value <<=1;	
+	     if(i<12) value <<=1; // Calcule les 12 premiers bits	
 	     if( digitalRead(MOSI)) 
 	        { 
 		 if(i<12) value |=1;
